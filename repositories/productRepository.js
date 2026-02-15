df54eefb15cfaee64eb8e59422eb56d423c65a51
Vinicius Almeida Andrade <vini3104@hotmail.com>
2026-02-15 18:29:01 -0300

    feat: Adicionar findAll para filtros do query params

--- repositories/productRepository.js
+++ repositories/productRepository.js
@@ -1,21 +1,11 @@
 let produtos = [
     {id: 1, nome: "Camisa", preco: 80, estoque: 70 },
-    { id: 2, nome: "Calça", preco: 200, estoque: 50 },
+    { id: 2, nome: "Calça", preco: 200, estoque: 0 },
     { id: 3, nome: "short", preco: 130, estoque: 20 }
 ];
 
-function findAll(){
-
-    let select = produtos.filter(p => p.estoque > 0)
-    return select;
-};
-
-function findById(id_recebido){
-    const select = produtos.find(p => p.id === id_recebido);
-    return select;
-};
-
-function create(product){
+function createProduct(data){
+    const { nome, preco, estoque } = data; 
     const novoProduto = {
         id: produtos.length + 1,
         nome,
@@ -24,12 +14,44 @@ function create(product){
     };
 
     produtos.push(novoProduto);
+    return novoProduto;
 
+};
+
+function findAll(filtros = {}){
+    let resultado = produtos
+    
+    if (filtros.nome) {
+        resultado = resultado.filter(p => 
+            p.nome.toLowerCase().includes(filtros.nome.toLowerCase())
+        );
+    };
+    
+    if (filtros.estoque === "true") {
+        resultado = resultado.filter(p => p.estoque > 0);
+    };
+
+    if (filtros.minpreco) {
+        resultado = resultado.filter(p => p.preco >= Number(filtros.minpreco)
+        );
+    };
+
+    if (filtros.maxpreco) {
+        resultado = resultado.filter(p => p.preco <= Number(filtros.maxpreco)
+        );
+    };
+
+    return resultado;
+};
+
+function findById(id_recebido){
+    console.log('3. ID no repository:', id_recebido);
+    return produtos.find(p => p.id === id_recebido);
 };
 
 
 module.exports = {
+    createProduct,
     findAll,
-    findById,
-    create
+    findById
 }
