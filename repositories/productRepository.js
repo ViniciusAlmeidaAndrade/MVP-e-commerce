1a3c6a2561aff1e585644a083bbbf6f3cc110a76
Vinicius Almeida Andrade <vini3104@hotmail.com>
2026-02-20 23:59:49 -0300

    feat: Ajustar

--- repositories/productRepository.js
+++ repositories/productRepository.js
@@ -1,57 +1,114 @@
-let produtos = [
-    {id: 1, nome: "Camisa", preco: 80, estoque: 70 },
+let products = [
+    {id: 1, nome: "Camisa", preco: 200, estoque: 70 },
     { id: 2, nome: "Calça", preco: 200, estoque: 0 },
     { id: 3, nome: "short", preco: 130, estoque: 20 }
 ];
 
-function createProduct(data){
+function create(data){
     const { nome, preco, estoque } = data; 
     const novoProduto = {
-        id: produtos.length + 1,
+        id: products.length + 1,
         nome,
         preco,
         estoque
     };
 
-    produtos.push(novoProduto);
+    products.push(novoProduto);
     return novoProduto;
 
 };
 
-function findAll(filtros = {}){
-    let resultado = produtos
+function applyFilter(resultado, filters){
     
-    if (filtros.nome) {
+    if (filters.nome) {
         resultado = resultado.filter(p => 
-            p.nome.toLowerCase().includes(filtros.nome.toLowerCase())
+            p.nome.toLowerCase().includes(filters.nome.toLowerCase())
         );
     };
     
-    if (filtros.estoque === "true") {
+    if (filters.estoque === "true") {
         resultado = resultado.filter(p => p.estoque > 0);
     };
 
-    if (filtros.minpreco) {
-        resultado = resultado.filter(p => p.preco >= Number(filtros.minpreco)
+    if (filters.minpreco) {
+        resultado = resultado.filter(p => p.preco >= Number(filters.minpreco)
         );
     };
 
-    if (filtros.maxpreco) {
-        resultado = resultado.filter(p => p.preco <= Number(filtros.maxpreco)
+    if (filters.maxpreco) {
+        resultado = resultado.filter(p => p.preco <= Number(filters.maxpreco)
         );
     };
 
-    return resultado;
+    if (filters.sort === "preco") {
+        if (filters.order === "asc" || filters.order != "asc" || filters.order != "desc") {
+            resultado.sort((a, b) => a.preco - b.preco);
+        };
+    
+        if (filters.order === "desc") {
+            resultado.sort((a, b) => b.preco - a.preco);
+        };
+    };
+
+    return resultado
 };
 
-function findById(id_recebido){
-    console.log('3. ID no repository:', id_recebido);
-    return produtos.find(p => p.id === id_recebido);
+function applySort(resultado, filters){
+    
+    if (filters.sort === "preco") {
+        if (filters.order === "asc" || filters.order != "asc" || filters.order != "desc") {
+            resultado.sort((a, b) => a.preco - b.preco);
+        };
+    
+        if (filters.order === "desc") {
+            resultado.sort((a, b) => b.preco - a.preco);
+        };
+    };
+
+    return resultado
+}
+
+function applyPagination(resultado, page, limit){
+    const offset = (page -1) * limit;
+
+    return resultado.slice(offset, offset + limit);
+}
+
+function findAll (filters ={}){
+    let resultado = products;
+    
+    resultado = applyFilter(resultado, filters);
+    resultado = applySort(resultado, filters);
+    
+    const total = resultado.length;
+
+    const page = Number(filters.page) || 1;
+    const limit = Number(filters.limit) || 10;
+    
+    resultado = applyPagination(resultado, page, limit);
+
+    const totalPages = Math.ceil(total / limit);
+    
+    return {
+        data: resultado,
+        page,
+        limit,
+        total,
+        totalPages
+    };
+};
+
+function findById(id){
+    console.log('3. ID no repository:', id);
+    return products.find(p => p.id === id);
 };
 
 
 module.exports = {
-    createProduct,
+    create,
+    applyFilter,
+    applySort,
+    applyPagination,
     findAll,
     findById
-}
+};
